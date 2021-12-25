525b516268cd45634dfa4a8779c805de253df1d4
tokopo <94882217+tokopo@users.noreply.github.com>
2021-12-25 15:35:29 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+let display = document.getElementById("display");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+let hours = 0;
+let minutes = 0;
+let seconds =0;
+let aSecond =0;
+
+let status;
+let interval;
+
+function stopWatch(){
+ aSecond++;
+ if(aSecond /10 == 1){
+   seconds++;
+   aSecond = 0;
+  
+   if(seconds / 60 == 1){
+     minutes++;
+     seconds = 0;
+
+     if(minutes / 60 == 1){
+       hours++;
+       minutes = 0;
+ 
+ 
+ 
+      }
+  }
+  }
+
+
+  display.innerHTML = hours + ":" + minutes + ":" + seconds + ":" + aSecond; 
+}
+
+start.addEventListener("click", function(){
+   interval = setInterval(stopWatch, 100);
+  }
+  )
+
+
+
+
+
+
+
+  stop.addEventListener("click", function(){
+  clearInterval(interval);
+}
+)
+
+reset.addEventListener("click", function(){
+  clearInterval(interval);
+  display.innerHTML = "0:0:0:0";
+}
+)
+
+
+
